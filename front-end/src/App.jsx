a0c46a3ebe401f26879cd5b7f3e6ab4a048d8ede
ngan <vutran@sheridancollege.ca>
2024-06-20 22:40:40 -0400

    Test Multer

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,3 +1,18 @@
-function App() {}
+function App() {
+  function handleChange(e) {
+    console.log(e.target.files[0])
+  }
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="file" name="file" id="file" onChange={handleChange} />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
 
 export default App
