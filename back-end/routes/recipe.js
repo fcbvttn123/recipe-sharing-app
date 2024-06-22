eaa4d7ed50a321f1157d7b2c0a9a5b66b175d21a
ngan <vutran@sheridancollege.ca>
2024-06-22 12:42:52 -0400

    Multer Test is successful

--- back-end/routes/recipe.js
+++ back-end/routes/recipe.js
@@ -1,22 +1,43 @@
 const express = require("express")
+const multer = require("multer")
 const router = express.Router()
+const path = require("path")
+const fs = require("fs")
 
+// Ensure the uploads directory exists
+const uploadDir = path.join(__dirname, "../images")
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true })
+}
+
+// Multer configuration for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, uploadDir)
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname)
+  },
+})
+
+// Middleware
+const upload = multer({ storage: storage })
+
+// Routes
 router.get("/", (req, res) => {
   res.json({ message: "GET recipes" })
 })
-
 router.get("/:id", (req, res) => {
   res.json({ message: `GET recipe: ${req.params.id}` })
 })
-
-router.post("/", (req, res) => {
+router.post("/", upload.single("image"), (req, res) => {
+  console.log(req.file.filename)
+  console.log(req.body.title)
   res.json({ message: "POST recipes" })
 })
-
 router.delete("/:id", (req, res) => {
   res.json({ message: `DELETE recipe: ${req.params.id}` })
 })
-
 router.patch("/:id", (req, res) => {
   res.json({ message: `UPDATE recipe: ${req.params.id}` })
 })
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [file, setFile] = useState(null)
+  const [title, setTitle] = useState("")
   function handleChange(e) {
-    console.log(e.target.files[0])
+    setFile(e.target.files[0])
   }
   function handleSubmit(e) {
     e.preventDefault()
+    const formData = new FormData()
+    formData.append("title", title)
+    formData.append("image", file)
+    async function post() {
+      let res = await fetch("/api/recipe", {
+        method: "POST",
+        body: formData,
+      })
+      let json = await res.json()
+      console.log(json)
+    }
+    post()
   }
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="file" name="file" id="file" onChange={handleChange} />
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+          required
+        />
+        <input type="file" name="image" onChange={handleChange} />
         <button>Submit</button>
       </form>
     </div>
