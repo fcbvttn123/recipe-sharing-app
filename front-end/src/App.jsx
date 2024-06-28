9b7bf350935f49e4f48ad4e186fa2cca768176cd
ngan <vutran@sheridancollege.ca>
2024-06-28 14:28:47 -0400

    Packages Installation for front-end

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,41 +1,7 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 function App() {
-  const [file, setFile] = useState(null)
-  const [title, setTitle] = useState("")
-  function handleChange(e) {
-    setFile(e.target.files[0])
-  }
-  function handleSubmit(e) {
-    e.preventDefault()
-    const formData = new FormData()
-    formData.append("title", title)
-    formData.append("image", file)
-    async function post() {
-      let res = await fetch("/api/recipe", {
-        method: "POST",
-        body: formData,
-      })
-      let json = await res.json()
-      console.log(json)
-    }
-    post()
-  }
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Title"
-          required
-        />
-        <input type="file" name="image" onChange={handleChange} />
-        <button>Submit</button>
-      </form>
-    </div>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default App
