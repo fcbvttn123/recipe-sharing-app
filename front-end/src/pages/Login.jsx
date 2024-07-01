dc17f4b8fa28d3dff6b3839c20892fc6e0371fb3
ngan <vutran@sheridancollege.ca>
2024-06-30 20:14:49 -0400

    Call /api/auth/login successfully

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,14 +1,54 @@
 import { Button, TextField, Typography } from "@material-ui/core"
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { handleFormChange } from "../hooks/handleFormChange"
+import { useAuthContext } from "../hooks/useAuthContext"
+import { AUTH_ACTIONS } from "../main"
 
 export function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const { user, dispatch } = useAuthContext()
+  const navigate = useNavigate()
   function handleFormSubmit(e) {
     e.preventDefault()
-    console.log("Login")
+    async function startLogin(email, password) {
+      setIsLoading(true)
+      try {
+        let res = await fetch("/api/auth/login", {
+          method: "POST",
+          body: JSON.stringify({ email, password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        let json = await res.json()
+        if (!res.ok) {
+          setError(json.error)
+          setIsLoading(false)
+        }
+        if (res.ok) {
+          localStorage.setItem(
+            "RECIPE-SHARING-APP-USER-TOKEN",
+            JSON.stringify(json)
+          )
+          dispatch({ type: AUTH_ACTIONS.LOGIN, payload: json })
+          setIsLoading(false)
+          navigate("/")
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    startLogin(formData.email, formData.password)
   }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      {/* Sign Up Text */}
+      {/* Login Text */}
       <Typography variant="h3" color="primary" gutterBottom>
         Login
       </Typography>
@@ -17,8 +57,23 @@ export function Login() {
         onSubmit={handleFormSubmit}
         className="flex flex-col items-center gap-y-5 mb-3"
       >
-        <TextField id="filled-basic" label="Email" variant="filled" />
-        <TextField id="filled-basic" label="Password" variant="filled" />
+        <TextField
+          id="filled-basic email"
+          label="Email"
+          variant="filled"
+          name="email"
+          value={formData.email}
+          onChange={(e) => handleFormChange(e, setFormData)}
+        />
+        <TextField
+          id="filled-basic password"
+          label="Password"
+          variant="filled"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={(e) => handleFormChange(e, setFormData)}
+        />
         <Button
           variant="contained"
           color="primary"
@@ -29,7 +84,7 @@ export function Login() {
         </Button>
       </form>
       {/* Form Error Text */}
-      <p className="text-red-600">Form Error</p>
+      {error && <p className="text-red-600">{error}</p>}
       {/* Back Button */}
       <div className="absolute left-5 top-5">
         <Link to={".."} relative="path">
