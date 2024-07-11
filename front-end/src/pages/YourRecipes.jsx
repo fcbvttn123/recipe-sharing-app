1f26379e44393f9959c7f2e32eedb016b67a8d0c
ngan <vutran@sheridancollege.ca>
2024-07-11 11:12:53 -0400

    When user does not have any recipe, render text 'No recipes found for this email'

--- front-end/src/pages/YourRecipes.jsx
+++ front-end/src/pages/YourRecipes.jsx
@@ -4,8 +4,48 @@ import { RecipeCard } from "../components/RecipeCard"
 import { CircularProgress } from "@material-ui/core"
 
 export function YourRecipes() {
+  /* 
+    States
+  */
   const { user } = useAuthContext()
   const [recipes, setRecipes] = useState(null)
+
+  /* 
+    This code is used to render content conditionally 
+  */
+  let content = null
+  if (recipes?.message == "No recipes found for this email") {
+    // If user does not have any recipes, render text to screen
+    console.log(recipes)
+    content = <p>No recipes found for this email</p>
+    // If user has recipes, render recipe cards
+  } else if (recipes?.length > 0) {
+    console.log(recipes)
+    content = recipes.map((e) => (
+      <RecipeCard
+        key={e._id}
+        id={e._id}
+        email={e.email}
+        title={e.title}
+        datePosted={e.createdAt}
+        imgName={e.image}
+        ingredients={e.ingredients}
+        instructions={e.instruction}
+        likedBy={e.likedBy}
+        likeNumber={e.likedBy.length}
+        showDeleteIcon={true}
+        showVerticalDotsIcon={true}
+      />
+    ))
+    // If the fetch process is loading, render spinning animation
+  } else {
+    console.log(recipes)
+    content = <CircularProgress />
+  }
+
+  /* 
+    This useEffect() function is used to fetch recipes from one user
+  */
   useEffect(() => {
     async function getYourRecipes(token) {
       try {
@@ -24,28 +64,10 @@ export function YourRecipes() {
     }
     getYourRecipes(user.token)
   }, [])
+
   return (
     <div className="flex flex-wrap justify-center items-center gap-8">
-      {recipes ? (
-        recipes.map((e) => (
-          <RecipeCard
-            key={e._id}
-            id={e._id}
-            email={e.email}
-            title={e.title}
-            datePosted={e.createdAt}
-            imgName={e.image}
-            ingredients={e.ingredients}
-            instructions={e.instruction}
-            likedBy={e.likedBy}
-            likeNumber={e.likedBy.length}
-            showDeleteIcon={true}
-            showVerticalDotsIcon={true}
-          />
-        ))
-      ) : (
-        <CircularProgress />
-      )}
+      {content}
     </div>
   )
 }
