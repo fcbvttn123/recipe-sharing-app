e88643e9ac8ed48183c9ea654e9b14af13eeeaf0
ngan <vutran@sheridancollege.ca>
2024-07-13 10:59:37 -0400

    Create stream token after user sign-up process

--- back-end/controllers/userControllers.js
+++ back-end/controllers/userControllers.js
@@ -1,5 +1,15 @@
 const jwt = require("jsonwebtoken")
 const User = require("../models/userModels")
+const StreamChat = require("stream-chat").StreamChat
+
+function createStreamToken(_id) {
+  const serverClient = new StreamChat(
+    "d8t3tcvbutju",
+    "4yz8prf2smx6tycskkyvus5jesnx2q7jxsvpex5y6jm6b58hv9z58sbuf3vj3n2v"
+  )
+  const token = serverClient.createToken(_id)
+  return token
+}
 
 function createToken(_id) {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" })
@@ -10,7 +20,8 @@ async function signupUser(req, res) {
   try {
     const user = await User.signup(email, password)
     const token = createToken(user._id)
-    res.status(200).json({ email, token })
+    const streamToken = createStreamToken(user.email)
+    res.status(200).json({ email, token, streamToken })
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
