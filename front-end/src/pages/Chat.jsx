b3a2fa6fb11f74ecef082dbf4fe1ae571b66e8fb
ngan <vutran@sheridancollege.ca>
2024-07-14 13:06:15 -0400

    Render all emails in Frontend Drawer

--- front-end/src/pages/Chat.jsx
+++ front-end/src/pages/Chat.jsx
@@ -1,8 +1,48 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useAuthContext } from "../hooks/useAuthContext"
+import {
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  makeStyles,
+} from "@material-ui/core"
+
+const drawerWidth = 280
+const useStyles = makeStyles((theme) => ({
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+}))
 
 export function Chat() {
   const { user } = useAuthContext()
+  const [emails, setEmails] = useState(null)
+  const classes = useStyles()
+  const drawer = (
+    <div>
+      <List></List>
+      <Divider />
+      {emails &&
+        emails.map((e) => (
+          <>
+            <List>
+              <ListItem button>
+                <ListItemText primary={e.email} />
+              </ListItem>
+            </List>
+            <Divider />
+          </>
+        ))}
+    </div>
+  )
   useEffect(() => {
     async function getAllEmails(token) {
       let res = await fetch("/api/auth/getAllEmails", {
@@ -11,13 +51,21 @@ export function Chat() {
         },
       })
       let json = await res.json()
-      console.log(json)
+      setEmails(json)
     }
     user?.token && getAllEmails(user.token)
   }, [])
   return (
     <div>
-      <p>chat</p>
+      <Drawer
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        variant="permanent"
+        open
+      >
+        {drawer}
+      </Drawer>
     </div>
   )
 }
